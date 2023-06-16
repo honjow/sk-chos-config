aade375d38a60eda53c741477e05a6e65c90be23
honjow <honjow311@gmail.com>
2023-06-16 21:39:35 +0800

    标题

--- src/main/sk-holoiso-config.py
+++ src/main/sk-holoiso-config.py
@@ -30,21 +30,21 @@ class SkHoloisoConfigApp(Gtk.Application):
 
         window.set_icon_name("logisim")
 
-        # scrolled_window = Gtk.ScrolledWindow()
-        # scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        # window.add(scrolled_window)
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        window.add(scrolled_window)
 
-        # header_bar = Gtk.HeaderBar()
-        # header_bar.set_show_close_button(True)
-        # window.set_titlebar(header_bar)
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_show_close_button(True)
+        window.set_titlebar(header_bar)
 
         # 创建垂直布局容器
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
         vbox.set_margin_start(20)
         vbox.set_margin_end(20)
-        vbox.set_margin_top(20)
+        vbox.set_margin_top(10)
         vbox.set_margin_bottom(20)
-        window.add(vbox)
+        scrolled_window.add(vbox)
 
         # 创建堆栈容器
         stack = Gtk.Stack()
@@ -57,8 +57,16 @@ class SkHoloisoConfigApp(Gtk.Application):
         stack_switcher.set_halign(Gtk.Align.CENTER)
         stack_switcher.set_valign(Gtk.Align.CENTER)
 
-        # 将堆栈切换器和堆栈容器添加到垂直布局容器中
-        vbox.pack_start(stack_switcher, False, True, 0)
+        # 堆栈切换器和堆栈容器添加
+        # header_bar.pack_start(stack_switcher)
+        # label = Gtk.Label()
+        header_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        header_box.pack_start(stack_switcher, False, False, 0)
+        # header_box.pack_start(label, True, True, 0)
+
+        header_bar.set_custom_title(header_box)
+
+        # vbox.pack_start(stack_switcher, False, False, 0)
         vbox.pack_start(stack, True, True, 0)
 
         # 相关功能开关
@@ -66,25 +74,22 @@ class SkHoloisoConfigApp(Gtk.Application):
         switch_box.set_margin_bottom(10)
         # vbox.pack_start(switch_box, True, True, 0)
 
-        # label1 = Gtk.Label()
-        # label1.set_markup("<b>功能开关</b>")
-        # label1.set_halign(Gtk.Align.START)
-        # switch_box.pack_start(label1, False, False, 0)
-
         stack.add_titled(switch_box, "switch", "功能开关")
 
         handycon_enabled = check_service_autostart("handycon.service")
-        function_switch_handycon = SwitchItem("HandyGCCS", "用来驱动部分掌机的手柄按钮", handycon_enabled, install.handycon_switch_callback)
-        switch_box.pack_start(function_switch_handycon, False, False, 0)
+        switch_item_handycon = SwitchItem("HandyGCCS", "用来驱动部分掌机的手柄按钮", handycon_enabled, install.handycon_switch_callback)
+        switch_box.pack_start(switch_item_handycon, False, False, 0)
 
         if self.product_name == "ONEXPLAYER 2 ARP23":
             oxp2lsusb_enabled = check_service_autostart("oxp2-lsusb.service")
-            function_switch_oxp2lsusb = SwitchItem("OXP2手柄热插拔检测修复", "修复OXP2手柄热插拔后不识别的问题", oxp2lsusb_enabled, install.oxp2lsusb_switch_callback)
-            switch_box.pack_start(function_switch_oxp2lsusb, False, False, 0)
+            switch_item_oxp2lsusb = SwitchItem("OXP2手柄热插拔检测修复", "修复OXP2手柄热插拔后不识别的问题", oxp2lsusb_enabled, install.oxp2lsusb_switch_callback)
+            switch_box.pack_start(switch_item_oxp2lsusb, False, False, 0)
+
 
             oxp2_volume_button_fix_enabled = check_service_autostart("oxp2-volume-button-fix.service")
-            function_switch_oxp2_volume_button_fix = SwitchItem("OXP2音量键修复", "修复OXP2音量键问题", oxp2_volume_button_fix_enabled, install.oxp2_volume_button_fix_switch_callback)
-            switch_box.pack_start(function_switch_oxp2_volume_button_fix, False, False, 0)
+            switch_item_oxp2_volume_button_fix = SwitchItem("OXP2音量键修复", "修复OXP2音量键问题", oxp2_volume_button_fix_enabled, install.oxp2_volume_button_fix_switch_callback)
+            switch_box.pack_start(switch_item_oxp2_volume_button_fix, False, False, 0)
+ 
 
         if self.product_name in (
             "NEXT",
@@ -103,12 +108,12 @@ class SkHoloisoConfigApp(Gtk.Application):
             aya_lc_suspend_file = '/usr/share/handygccs/aya-lc-suspend'
             aya_lc_suspend_enabled = os.path.isfile(aya_lc_suspend_file)
             # aya_lc_suspend_enabled = False
-            function_switch_aya_lc_suspend = SwitchItem("AYANEO LC键睡眠", "默认为截图, 开启后LC键作为睡眠键", aya_lc_suspend_enabled, install.aya_lc_suspend_switch_callback)
-            switch_box.pack_start(function_switch_aya_lc_suspend, False, False, 0)
+            switch_item_aya_lc_suspend = SwitchItem("AYANEO LC键睡眠", "默认为截图, 开启后LC键作为睡眠键", aya_lc_suspend_enabled, install.aya_lc_suspend_switch_callback)
+            switch_box.pack_start(switch_item_aya_lc_suspend, False, False, 0)
+
 
         # 手动更新
         update_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        update_box.set_margin_top(10)
 
         # stack.add_titled(update_box, "update", "手动更新")
 
@@ -130,7 +135,6 @@ class SkHoloisoConfigApp(Gtk.Application):
 
         # 管理
         manager_page = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        manager_page.set_margin_top(10)
         stack.add_titled(manager_page, "manager", "安装管理")
 
         def handycon_installed():
@@ -149,7 +153,7 @@ class SkHoloisoConfigApp(Gtk.Application):
         manager_page.pack_start(item_power_control, False, False, 0)
 
         def tomoon_installed():
-            return os.path.isfile(os.path.expanduser("~/homebrew/plugins/toMoon/plugin.json"))
+            return os.path.isfile(os.path.expanduser("~/homebrew/plugins/tomoon/plugin.json"))
         item_tomoon = ManagerItem("Tomoon", "科学上网Decky插件", tomoon_installed, install.tomoon_install)
         manager_page.pack_start(item_tomoon, False, False, 0)
 
@@ -157,14 +161,14 @@ class SkHoloisoConfigApp(Gtk.Application):
         manager_page.pack_start(item_this_app, False, False, 0)
 
 
-        def test_installed():
-            return True
-        def test_install():
-            time.sleep(2)
-        def test_uninstall():
-            time.sleep(2)
-        item_test = ManagerItem("Test", "测试项目", True, test_install, test_uninstall)
-        manager_page.pack_start(item_test, False, False, 0)
+        # def test_installed():
+        #     return True
+        # def test_install():
+        #     time.sleep(2)
+        # def test_uninstall():
+        #     time.sleep(2)
+        # item_test = ManagerItem("Test", "测试项目", True, test_install, test_uninstall)
+        # manager_page.pack_start(item_test, False, False, 0)
 
         # 关于
         stack.add_titled(AboutPage(), "about", "关于")
