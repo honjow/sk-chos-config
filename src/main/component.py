05c22da27eea65928254520e287e047c07b0b35f
honjow <honjow311@gmail.com>
2023-06-16 20:20:36 +0800

    调整布局

--- src/main/component.py
+++ src/main/component.py
@@ -9,7 +9,7 @@ import threading
 from gi.repository import GLib, Gtk, Pango, Gdk
 
 
-class FunctionSwitch(Gtk.Box):
+class SwitchItem(Gtk.Box):
     def __init__(self, title, description, initial_value=False, callback=None):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
         self.set_margin_start(5)
@@ -60,7 +60,121 @@ class FunctionSwitch(Gtk.Box):
             self.callback(active)
             self.last_value = active
 
-class UpdateButton(Gtk.Button):
+
+class ManagerItem(Gtk.Box):
+    def __init__(self, title, description, installed_cb, install_callback=None, uninstall_callback=None):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
+        self.set_margin_start(5)
+        self.set_margin_end(5)
+        self.set_margin_top(10)
+        self.set_margin_bottom(10)
+
+        self.install_callback = install_callback
+        self.uninstall_callback = uninstall_callback
+        self.install_button = None
+        self.uninstall_button = None
+        
+        self.installed_cb = installed_cb
+
+        if callable(self.installed_cb):
+            self.current_installed = self.installed_cb()
+        else:
+            self.current_installed = self.installed_cb
+
+        # 左边文字部分
+        left_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        self.pack_start(left_box, True, True, 0)
+
+        switch_label = Gtk.Label()
+        switch_label.set_text(title)
+        switch_label.set_halign(Gtk.Align.START)
+        switch_label.set_valign(Gtk.Align.START)
+        left_box.pack_start(switch_label, False, False, 0)
+
+        desc_label = Gtk.Label()
+        desc_label.set_text(description)
+        desc_label.set_halign(Gtk.Align.START)
+        desc_label.set_valign(Gtk.Align.START)
+        desc_label.set_xalign(0)
+        desc_label.set_yalign(0)
+        desc_label.set_line_wrap(True)
+        desc_label.set_line_wrap_mode(Pango.WrapMode.WORD)
+        desc_label.set_ellipsize(Pango.EllipsizeMode.NONE)
+        desc_label.set_markup("<small>" + desc_label.get_text() + "</small>")
+        left_box.pack_start(desc_label, False, False, 0)
+
+        self.spinner = Gtk.Spinner()
+        self.spinner.set_valign(Gtk.Align.CENTER)
+        self.spinner.set_margin_end(10)
+        self.pack_start(self.spinner, False, False, 0)
+
+        # 右边按钮部分
+        if self.uninstall_callback is not None:
+            self.uninstall_button = Gtk.Button()
+            self.uninstall_button.set_margin_end(10)
+            self.uninstall_button.set_label("卸载")
+            self.uninstall_button.set_valign(Gtk.Align.CENTER)
+            self.uninstall_button.connect("clicked", self.on_uninstall_clicked)
+            self.pack_start(self.uninstall_button, False, False, 0)
+
+        if self.install_callback is not None:
+            self.install_button = Gtk.Button()
+            self.install_button.set_label("更新" if installed_cb else "安装")
+            self.install_button.set_valign(Gtk.Align.CENTER)
+            self.install_button.connect("clicked", self.on_install_clicked)
+            self.pack_start(self.install_button, False, False, 0)
+
+    def disable_buttons(self):
+        if self.install_button is not None:
+            self.install_button.set_sensitive(False)
+        if self.uninstall_button is not None:
+            self.uninstall_button.set_sensitive(False)
+        self.spinner.start()
+
+    def enable_buttons(self):
+        if self.install_button is not None:
+            self.install_button.set_sensitive(True)
+        if self.uninstall_button is not None:
+            self.uninstall_button.set_sensitive(True)
+        self.spinner.stop()
+
+    def reload_installed(self):
+        if callable(self.installed_cb):
+            self.current_installed = self.installed_cb()
+        else:
+            self.current_installed = self.installed_cb
+    
+        if self.current_installed:
+            if self.install_button is not None:
+                self.install_button.set_label("更新")
+            if self.uninstall_button is not None:
+                self.uninstall_button.show()
+        else:
+            if self.install_button is not None:
+                self.install_button.set_label("安装")
+            if self.uninstall_button is not None:
+                self.uninstall_button.hide()
+
+    def completed(self, success, msg=None):
+        self.reload_installed()
+        self.enable_buttons()
+        print ("Completed: " + str(success) + " " + str(msg))
+
+    def execute_callback(self, callback):
+        result, ret_msg = callback()
+        GLib.idle_add(self.completed, result, ret_msg)
+
+    def on_install_clicked(self, button):
+        print("Installing...")
+        self.disable_buttons()
+        threading.Thread(target=self.execute_callback, args=(self.install_callback,)).start()
+
+    def on_uninstall_clicked(self, button):
+        print("Uninstalling...")
+        self.disable_buttons()
+        threading.Thread(target=self.execute_callback, args=(self.uninstall_callback,)).start()
+
+class UpdateFullButton(Gtk.Button):
     def __init__(self, title, callback=None):
         Gtk.Button.__init__(self, label=title)
 
