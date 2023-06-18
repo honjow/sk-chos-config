98df118c759e28050c3664651bb72af1ef70eee6
honjow <honjow311@gmail.com>
2023-06-18 17:11:48 +0800

    mesa install

--- src/main/install_mesa_arch.sh
+++ src/main/install_mesa_arch.sh
@@ -3,3 +3,5 @@ sudo pacman --config /etc/arch_pacman.conf --noconfirm -Sy \
 llvm llvm-libs lib32-llvm-libs \
 mesa libva-mesa-driver mesa-utils mesa-vdpau opencl-mesa vulkan-intel vulkan-radeon \
 lib32-mesa lib32-libva-mesa-driver lib32-mesa-vdpau lib32-vulkan-intel lib32-vulkan-radeon
+
+sudo pacman -Syy --noconfirm
--- src/main/install_mesa_valve.sh
+++ src/main/install_mesa_valve.sh
@@ -2,3 +2,5 @@
 sudo pacman --config /etc/valve_main_pacman.conf --noconfirm -Sy \
 mesa libva-mesa-driver mesa-utils mesa-vdpau opencl-mesa vulkan-intel vulkan-radeon \
 lib32-mesa lib32-libva-mesa-driver lib32-mesa-vdpau lib32-vulkan-intel lib32-vulkan-radeon
+
+sudo pacman -Syy --noconfirm
