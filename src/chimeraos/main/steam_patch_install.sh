51e45bcc6baa82630db8fe6eec59a11b768eb266
honjow <honjow311@gamil.com>
2023-10-06 06:53:48 +0800

    fix github_prefix

--- src/chimeraos/main/steam_patch_install.sh
+++ src/chimeraos/main/steam_patch_install.sh
@@ -2,5 +2,5 @@
 github_prefix=$1
 echo $github_prefix
 yay -Sy ryzenadj-git jq --needed --noconfirm && \
-curl -o /tmp/sp_install_cn.sh -L "${github_url}https://github.com/honjow/steam-patch/releases/latest/download/install_cn.sh" && \
+curl -o /tmp/sp_install_cn.sh -L "${github_prefix}https://github.com/honjow/steam-patch/releases/latest/download/install_cn.sh" && \
 chmod +x /tmp/sp_install_cn.sh && /tmp/sp_install_cn.sh $github_prefix
