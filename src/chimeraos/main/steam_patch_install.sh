2607e8c44a7dfc9d3fad38ba1dc3250108896977
honjow <honjow311@gmail.com>
2023-10-24 21:03:26 +0800

    update steam patch install scripts

--- src/chimeraos/main/steam_patch_install.sh
+++ src/chimeraos/main/steam_patch_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 github_prefix=$1
 echo $github_prefix
-yay -Sy ryzenadj-git jq --needed --noconfirm && \
+# yay -Sy ryzenadj-git jq --needed --noconfirm && \
 curl -o /tmp/sp_install_cn.sh -L "${github_prefix}https://github.com/honjow/steam-patch/releases/latest/download/install_cn.sh" && \
 chmod +x /tmp/sp_install_cn.sh && /tmp/sp_install_cn.sh $github_prefix
