6e672be997d10789bf4c4c8026daeea1f9793e95
honjow <honjow311@gmail.com>
2023-11-22 21:15:42 +0800

    fix install

--- src/chimeraos/main/power_control_install.sh
+++ src/chimeraos/main/power_control_install.sh
@@ -12,12 +12,23 @@ echo "github_prefix: ${github_prefix}"
 temp=$(mktemp -d)
 
 # Download latest release
-RELEASE=$(curl -s '${github_prefix}https://api.github.com/repos/mengmeet/PowerControl/releases/latest')
+RELEASE=$(curl -s "${github_prefix}https://api.github.com/repos/mengmeet/PowerControl/releases/latest")
 RELEASE_VERSION=$(echo "$RELEASE" | jq -r '.tag_name')
 RELEASE_URL=$(echo "$RELEASE" | jq -r '.assets[0].browser_download_url')
-curl -L -o ${temp}/PowerControl.tar.gz "${github_prefix}$RELEASE_URL"
+
+if [ -z "$RELEASE_VERSION" ] || [ -z "$RELEASE_URL" ]; then
+  echo "Failed to get latest release info" >&2
+  exit 1
+fi
+
+curl -L -o ${temp}/PowerControl.tar.gz "${github_prefix}${RELEASE_URL}"
 
 echo "Installing PowerControl $RELEASE_VERSION"
+
+if [ ! -f ${temp}/PowerControl.tar.gz ]; then
+  echo "Failed to download PowerControl $RELEASE_VERSION" >&2
+  exit 1
+fi
 
 # remove old version
 chmod -R 777 ${HOME}/homebrew/plugins
