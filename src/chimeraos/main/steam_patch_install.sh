f8d1088feb0cb4373272b412fb29908007bc2d3b
honjow <honjow311@gmail.com>
2023-12-01 00:18:46 +0800

    add make dir

--- src/chimeraos/main/steam_patch_install.sh
+++ src/chimeraos/main/steam_patch_install.sh
@@ -5,6 +5,24 @@
 # curl -o /tmp/sp_install_cn.sh -L "${github_prefix}https://github.com/honjow/steam-patch/releases/latest/download/install_cn.sh" && \
 # chmod +x /tmp/sp_install_cn.sh && /tmp/sp_install_cn.sh $github_prefix
 
+# cant run this script as root
+if [ "$EUID" -eq 0 ]; then
+    echo "Please run this script as a normal user, not root."
+    exit 1
+fi
+
+OLD_DIR="$HOME/steam-patch"
+
+# 获取 $USER_DIR/steam-patch 的所属用户 如果是root， 则删除
+if [ -d "$OLD_DIR" ]; then
+    USER_DIR_OWNER=$(stat -c '%U' $OLD_DIR)
+    if [ "$USER_DIR_OWNER" == "root" ]; then
+        sudo rm -rf $OLD_DIR
+    fi
+fi
+
+mkdir -p $HOME/steam-patch
+
 sudo systemctl --user stop steam-patch 2> /dev/null
 sudo systemctl --user disable steam-patch 2> /dev/null
 
