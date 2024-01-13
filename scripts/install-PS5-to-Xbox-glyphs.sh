b9d4a774b84aeb6d5b9a025bad0d6482b75e321d
honjow <honjow311@gmail.com>
2024-01-14 06:38:15 +0800

    fix scripts

--- scripts/install-PS5-to-Xbox-glyphs.sh
+++ scripts/install-PS5-to-Xbox-glyphs.sh
@@ -19,6 +19,8 @@ if [ "$commit_id" != "" ]; then
     git checkout $commit_id
 fi
 
+sed -i 's/Legion Go - PS5 controller emulator to xbox glyphs/PS5 controller emulator to xbox glyphs/g' theme.json
+
 mkdir -p $HOME/homebrew/themes
 
 if [ -d $HOME/homebrew/themes/$theme_name ]; then
