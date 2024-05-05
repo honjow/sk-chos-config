bb9ad1473774a06a222c3aa5a71377dcd8a968df
honjow <honjow311@gmail.com>
2024-05-05 11:17:46 +0800

    rename

--- src/chimeraos-addon/share/sk-chos/completions/gen.sh
+++ src/chimeraos-addon/share/sk-chos/completions/gen.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+name=cjust
+
+file_name=_${name}.zsh
+
 set -e
 just --completions zsh > _just.zsh
-just --completions zsh > _skjust.zsh
+just --completions zsh > $file_name
 
-sed -i 's/#compdef just/#compdef skjust/' _skjust.zsh
+sed -i "s/#compdef just/#compdef $name/" $file_name
 
 just --completions bash > _just.bash
