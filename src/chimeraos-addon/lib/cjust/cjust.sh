d0da2a873d79c69443a74409fede8b3fbdb49c23
honjow <honjow311@gmail.com>
2024-05-06 13:55:16 +0800

    Add setup-waydroid just file

---
+++ src/chimeraos-addon/lib/cjust/cjust.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# shellcheck source=/dev/null
+# Import color formatting
+source /usr/lib/cjust/libcolors.sh
+# Import text formatting
+source /usr/lib/cjust/libformatting.sh
+# Import functionality for just
+source /usr/lib/cjust/libfunctions.sh
