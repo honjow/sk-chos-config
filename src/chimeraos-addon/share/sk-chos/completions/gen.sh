ee6098ba680ec92a2de34a9994da3578d02f1864
honjow <honjow311@gmail.com>
2024-09-11 14:42:48 +0800

    update just completions_file

--- src/chimeraos-addon/share/sk-chos/completions/gen.sh
+++ src/chimeraos-addon/share/sk-chos/completions/gen.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-name=cjust
+alias_name=cjust
 
-file_name=_${name}
+zsh_completions_file_name="_${alias_name}"
+bash_completions_file_name="_${alias_name}.bash"
 
 set -e
 just --completions zsh > _just
-just --completions zsh > $file_name
+just --completions zsh > $zsh_completions_file_name
 
-sed -i "s/#compdef just/#compdef $name/" $file_name
+sed -i "s/#compdef just/#compdef $alias_name/" $zsh_completions_file_name
+sed -i "s/_just/_$alias_name/" $zsh_completions_file_name
 
 just --completions bash > _just.bash
+just --completions bash > $bash_completions_file_name
+
+sed -i "s/_just/_$alias_name/" $bash_completions_file_name
